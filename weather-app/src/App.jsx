d631f3a2926ce9478b043acc171ef40dfd8727c9
Patrick Wojcik <70239167+Pwojcik2@users.noreply.github.com>
2024-10-16 13:57:17 -0400

    added object state, redone useEffect

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,52 +1,53 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import WeatherCard from './components/WeatherCard'
 
 function App() {
 
   const [locationSearch, setLocationSearch] = useState('London')
-  const [temperature, setTemperature] = useState()
-  const [count, setCount] = useState(0)
+  const [weatherObject, setWeatherObject] = useState({
+    location: '',
+    temperature: ''
+  })
   const [search, setSearch] = useState(false)
 
   const url = `http://api.weatherapi.com/v1/current.json?key=14d70ead84904419ada154523241610&q=${locationSearch}&aqi=yes`
 
-  const handleInput = (e) => {
+  const handleChange = (e) => {
     setLocationSearch(e.target.value)
   }
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(url)
-      result.json().then(json => {
-        setTemperature(json.current.temp_f)
+      const fetchData = async () => {
+        const result = await fetch(url)
+        const json = await result.json()
+        setWeatherObject({
+          location: json.location.name,
+          temperature: json.current.temp_f
+        })
         console.log(json)
         setSearch(false)
-      })
-    }
-    fetchData()
+      }
+      fetchData()
   }, [search])
 
   return (
     <>
       <h1>Weather App</h1>
+        <label>
+        Search Location: <input type="text" placeholder='Enter a City' onChange={handleChange}/>
+        </label>
 
-      <label>
-        Search Location: <input type="text" value={locationSearch} onChange={handleInput}/>
-      </label>
+      <div>
       <button onClick={() => setSearch(true)}>
           Search
         </button>
-
-      <h1>Current Temp: {temperature}</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count + {count}
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
-          count - {count}
-        </button>
       </div>
+
+      <WeatherCard />
+
+      <h1>Current Location: {weatherObject.location}</h1>
+      <h1>Current Temp: {weatherObject.temperature}°F</h1>
     </>
   )
 }
