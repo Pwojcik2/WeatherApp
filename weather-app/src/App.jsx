6c52426bec92332d3d0e2901f080e67a885a6107
Patrick Wojcik <70239167+Pwojcik2@users.noreply.github.com>
2024-10-16 12:42:30 -0400

    added api fetch call and input case

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  const [locationSearch, setLocationSearch] = useState('London')
+  const [temperature, setTemperature] = useState()
   const [count, setCount] = useState(0)
+  const [search, setSearch] = useState(false)
+
+  const url = `http://api.weatherapi.com/v1/current.json?key=14d70ead84904419ada154523241610&q=${locationSearch}&aqi=yes`
+
+  const handleInput = (e) => {
+    setLocationSearch(e.target.value)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(url)
+      result.json().then(json => {
+        setTemperature(json.current.temp_f)
+        console.log(json)
+        setSearch(false)
+      })
+    }
+    fetchData()
+  }, [search])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Weather App</h1>
+
+      <label>
+        Search Location: <input type="text" value={locationSearch} onChange={handleInput}/>
+      </label>
+      <button onClick={() => setSearch(true)}>
+          Search
+        </button>
+
+      <h1>Current Temp: {temperature}</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count + {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount((count) => count - 1)}>
+          count - {count}
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
